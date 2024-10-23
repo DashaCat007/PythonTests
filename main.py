07eaec45896657e7136b7ff322b1a30ea3176580
Dashka <podarochek1997@gmail.com>
2024-10-23 18:07:57 +0300

    Начало

---
+++ main.py
@@ -0,0 +1,9 @@
+print(2+3)
+print("hello")
+if 1<2:
+    print("hello")
+    # выводим на консоль Hello World
+    print("Hello World")
+    print("Full name:", "World","Python")
+    name=input("Введите имя: ")
+    print("Привет", name)

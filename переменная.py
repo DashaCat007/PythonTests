c67e45b02cc55be20a8f4006da0dec31809ae60b
Dashka <podarochek1997@gmail.com>
2024-10-28 12:06:54 +0300

    переменные

---
+++ переменная.py
@@ -0,0 +1,24 @@
+text=("Laudate omnes gentes laudate"
+    " Magnificat in secula")
+print(text)
+text='''Laudate omnes gentes laudate
+Magnificat in secula
+Et anima mea laudate
+Magnificat in secula '''
+print(text)
+text="Message:\n\"Hello World\""
+print(text)
+path= r"C:\python\name.txt"
+print(path)
+userName = "Tom"
+userAge = 37
+user = f"name: {userName} age: {userAge}"
+print(user) #name: Tom, age: 37
+userId = "abc" # тип str
+print(userId)
+userId = 234 #тип int
+print(userId)
+
+
+
+
